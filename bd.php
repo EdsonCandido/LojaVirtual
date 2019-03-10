fb3f6dd0e621c93fb2b44e8e4648cd7e1e710355
Edson Cândido Alves Júnior <edson.c.a.j@hotmail.com>
2019-03-10 00:08:01 -0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,17 +1,11 @@
 <?php 
-
-/*$host = "sql205.epizy.com";
-$user = "epiz_22096312";
-$pass = "SVb0Y4J4WtyC";
-$banco = "epiz_22096312_banco";*/
-
-$host = "superbay.mysql.dbaas.com.br";
-$user = "superbay";
-$pass = "superbay123";
-$banco = "superbay";
+//informações do seu banco de dados 
+$host = "locahost";
+$user = "admin";
+$pass = "admin";
+$banco = "loja_virtual";
 $conexao = mysql_connect($host, $user, $pass);
 mysql_select_db($banco);
 
 $urlSite = "http://localhost";
-
- ?>
+?>
