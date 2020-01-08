b4b03ae4637829009144850e51779d1ffaa803cb
Edson Cândido Alves Júnior <edson.c.a.j@hotmail.com>
2020-01-08 19:13:54 -0300

    Update bd.php

--- adm/bd.php
+++ adm/bd.php
@@ -1,13 +1,13 @@
 <?php 
 
-/*$host = "sql205.epizy.com";
-$user = "epiz_22096312";
-$pass = "SVb0Y4J4WtyC";
-$banco = "epiz_22096312_banco";*/
+/*$host = "";
+$user = "";
+$pass = "";
+$banco = "";*/
 
 $host = "superbay.mysql.dbaas.com.br";
 $user = "superbay";
-$pass = "superbay123";
+$pass = "";
 $banco = "superbay";
 $conexao = mysql_connect($host, $user, $pass);
 mysql_select_db($banco);
